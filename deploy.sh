dba81d210c2617345aac7134c224b6ce25fa1efc
Sunyxq <253832631@qq.com>
2020-06-08 08:57:47 +0800

    调整脚本

--- deploy.sh
+++ deploy.sh
@@ -14,7 +14,7 @@ git add -A
 git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io，去掉下面这一行的注释 blog
-git push -f https://046e3bcc90f79cd28b0dfb0fb5e476df4058eeec@github.com/sunyxq/sunyxq.github.io.git master
+git push -f https://${046e3bcc90f79cd28b0dfb0fb5e476df4058eeec}@github.com/sunyxq/sunyxq.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>，去掉下面这一行的注释
 # git push -f https://${token}@github.com:<USERNAME>/<REPO>.git master:gh-pages
